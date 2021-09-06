a3e1dda5350dc39fdfc84135008a052f17634f94
Krishna <skvkrishnamurthy@gmail.com>
2021-09-06 13:08:25 +0530

    Callbacks updated

--- Plots and Dashboard/Callbacks.py
+++ Plots and Dashboard/Callbacks.py
@@ -162,43 +162,69 @@
 #     app.run_server(debug=True)
 
 
+# import dash
+# from dash.dependencies import Input, Output
+# import dash_core_components as dcc
+# import dash_html_components as html
+
+# external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+# app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+# app.layout = html.Div(
+#     [
+#         dcc.Slider(
+#             id="slider-circular", min=0, max=50, 
+#             marks={i: str(i) for i in range(51)}, 
+#             value=3
+#         ),
+#         dcc.Input(
+#             id="input-circular", type="number", min=0, max=50, value=3
+#         ),
+#     ]
+# )
+
+# @app.callback(
+#     Output("input-circular", "value"),
+#     Output("slider-circular", "value"),
+#     Input("input-circular", "value"),
+#     Input("slider-circular", "value"),
+# )
+
+# def callback(input_value, slider_value):
+#     ctx = dash.callback_context
+#     trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
+#     value = input_value if trigger_id == "input-circular" else slider_value
+#     return value, value
+
+# if __name__ == '__main__':
+#     app.run_server(debug=True)
+
+
+
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input , Output
 import dash_core_components as dcc
 import dash_html_components as html
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
-
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__)
 
 app.layout = html.Div(
-    [
-        dcc.Slider(
-            id="slider-circular", min=0, max=50, 
-            marks={i: str(i) for i in range(51)}, 
-            value=3
-        ),
-        dcc.Input(
-            id="input-circular", type="number", min=0, max=50, value=3
-        ),
-    ]
-)
+    [html.H6("Changing the value in the output as in the input"),
+    html.Div([
+        "Input: ",
+        dcc.Input(id='my-input', value='initial value', type='text')
+    ]),
+    html.Br(),
+    html.Div(id='my-output'),
+])
 
 @app.callback(
-    Output("input-circular", "value"),
-    Output("slider-circular", "value"),
-    Input("input-circular", "value"),
-    Input("slider-circular", "value"),
+    Output(component_id="my-output",component_property="Children"),
+    Input(component_id="my-input", component_property="value")
 )
-
-def callback(input_value, slider_value):
-    ctx = dash.callback_context
-    trigger_id = ctx.triggered[0]["prop_id"].split(".")[0]
-    value = input_value if trigger_id == "input-circular" else slider_value
-    return value, value
+def update_output(input_value):
+    return 'Output:{}'.format(input_value)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
-
-
-
