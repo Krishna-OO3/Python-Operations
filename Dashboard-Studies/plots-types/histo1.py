e0b05abbb3744b1547605035908dccc3384e5da6
Krishna <skvkrishnamurthy@gmail.com>
2021-09-08 14:44:01 +0530

    Plot and dash basics

---
+++ Dashboard-Studies/plots-types/histo1.py
@@ -0,0 +1,16 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\vkrishnamurthy\Desktop\Git\Python-Operations\Dashboard-Studies\Data\mpg.csv')
+
+data = [go.Histogram(
+    x=df['mpg']
+)]
+
+layout = go.Layout(
+    title="Miles per Gallon Frequencies of<br>\
+    1970's Era Vehicles"
+)
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='basic-histogram.html')
